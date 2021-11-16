9e304ff355035a94616485b8e3db4e30b4990bbc
ivan3esoa <rurker2007@gmail.com>
2021-11-16 14:45:29 +0100

    tabla con for

--- bucles.py
+++ bucles.py
@@ -32,7 +32,7 @@ while a < b:
 #    propia rama del repositorio
 # 2. Resuelve el problema de la tabla de multiplicar.
 # 3. Puedes hacer "commit & push" para guardar los cambios en GitHub
-numero = 1 #pon aquí el número del que quieres hacer la tabla
+numero = int(input("pon un numero")) #pon aquí el número del que quieres hacer la tabla
 print("tabla de multiplicar del ", numero)
-
-
+for i in range (10):
+    print(numero,"por",i,"=",numero *i )
